edc073356ba6bfbf3e612a49b353e78b27fd8b6e
sonpal521 <119480099+sonpal521@users.noreply.github.com>
2022-11-30 12:22:36 +0530

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,29 @@
+
+function isPalindrome(n)
+{
+    // Find reverse of n
+    var rev = 0;
+    for (var i = n; Math.trunc(i) > 0; i /= 10)
+    {
+        rev = ((rev*10) + (Math.trunc(i)%10));
+          
+        }
+    
+    // If n and rev are same, then n is palindrome
+    return (n==rev);
+}
+      
+  
+// prints palindrome between min and max
+function countPal(min,  max)
+{
+    for (var i = min; i <=max; i++)
+    {
+        if(isPalindrome(i))
+        console.log(i+" " );
+       }
+}
+  
+
+  
+    countPal(1, 50);
